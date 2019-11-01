1bb2b7868d0fe6b053f73025c2f95c88a3bb841b
alet <alet@risepic.com>
2019-11-01 20:14:55 +0100

    0.512

--- spa/DFORule/view.jsx
+++ spa/DFORule/view.jsx
@@ -4,6 +4,13 @@ var DFORule = React.createClass({
             <div className="NavAll">
                     <div className="NavDeploy">
                     <h2><span className="BOLD">Hello World </span> | Deploy your DFO</h2>
+                    <section className="NavHow">
+                        <p>If you never heard about it, you have to can find more information here:</p>
+                        <h3><span className="BOLD">How to start a DFO:</span></h3>
+                        <p><span className="BOLD">STEP 1:</span> Define the voting rules and the voting token and deploy it to generate the DFO proxy Contract</p>
+                        <p><span className="BOLD">STEP 2:</span> Set your first function, deploying a Smart Contract and go to the section "Survey". Fill the label to create a survey and request the connection to the proxy to Token Holders.</p>
+                        <p><span className="BOLD">STEP 3:</span> Vote for the Survey using your Voting Tokens in the section "Governance"</p>
+                    </section>
                         <div className="special">
                             <h4 className="BOLD">DFO</h4>
                             <label for="dfoName">Name:</label>
--- spa/index/view.jsx
+++ spa/index/view.jsx
@@ -9,13 +9,6 @@ var Index = React.createClass({
                     <figure>
                         <div className="GIFY"><iframe src="https://giphy.com/embed/cPHYguVPkcXBIM5tpX" width="100%" height="100%" style={{"position":"absolute"}} frameBorder="0" className="giphy-embed" allowFullScreen></iframe></div>
                     </figure>
-                    <section className="MainHow">
-                        <p>If you never heard about it, you have to can find more information here:</p>
-                        <h3><span className="BOLD">How to start a DFO:</span></h3>
-                        <p><span className="BOLD">STEP 1:</span> Define the voting rules and the voting token and deploy it to generate the DFO proxy Contract</p>
-                        <p><span className="BOLD">STEP 2:</span> Set your first function, deploying a Smart Contract and go to the section "Survey". Fill the label to create a survey and request the connection to the proxy to Token Holders.</p>
-                        <p><span className="BOLD">STEP 3:</span> Vote for the Survey using your Voting Tokens in the section "Governance"</p>
-                    </section>
                     <div className="MainLoad">
                        <input id="dfo-address" placeholder="Enter your ethereum address here..." /> 
                        <button type="button" onclick="loadDFO();">Load</button>
