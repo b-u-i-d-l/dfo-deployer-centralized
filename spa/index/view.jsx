241bceaa2353fe0701462a94959ae25e4299504c
alet <alet@risepic.com>
2019-11-01 19:48:09 +0100

    0.51

--- spa/index/view.jsx
+++ spa/index/view.jsx
@@ -16,6 +16,10 @@ var Index = React.createClass({
                         <p><span className="BOLD">STEP 2:</span> Set your first function, deploying a Smart Contract and go to the section "Survey". Fill the label to create a survey and request the connection to the proxy to Token Holders.</p>
                         <p><span className="BOLD">STEP 3:</span> Vote for the Survey using your Voting Tokens in the section "Governance"</p>
                     </section>
+                    <div className="MainLoad">
+                       <input id="dfo-address" placeholder="Enter your ethereum address here..." /> 
+                       <button type="button" onclick="loadDFO();">Load</button>
+                    </div>
                 </article>
                 <section className="Nav">
                     <DFORule/>
