601aaeedac870438c95aa316b9d5761ff832fa78
alet89 <alet@risepic.com>
2019-11-02 16:51:30 +0100

    0.67

--- spa/run/view.jsx
+++ spa/run/view.jsx
@@ -9,15 +9,15 @@ var Run = React.createClass({
         var _this = this;
         return (
             <div className="chooser">
-                    <h2><span className="BOLD">Hello World </span> | Deploy your DFO</h2>
-                    <ul>
+                    <h2><span className="BOLD">The DFO </span> | Explore</h2>
+                    <ul className="NavRunRw">
                     {this.state && this.state.functionalities && this.state.functionalities.map(it =><li key={it.codeName}>
-                        <span>{it.codeName}</span>
-                        {!it.isInternal && it.submitable && <button onClick={e => _this.onClick(e, it)}>Write</button>}
-                        {!it.isInternal && !it.submitable && <button onClick={e => _this.onClick(e, it)}>Read</button>}
-                        {it.isInternal && <span>Internal</span>}
-                        <a href={getEtherscanURL() + "address/" + it.location}>Smart Contract</a>
-                        {it.proposalAddress && parseInt(it.proposalAddress.toLowerCase().split("x").join("")) >0 &&  <a href={getEtherscanURL() + "address/" + it.proposalAddress}>History</a>}
+                        <span className="RwCn">{it.codeName}</span>
+                        {!it.isInternal && it.submitable && <button className="RwWrite" onClick={e => _this.onClick(e, it)}>Write</button>}
+                        {!it.isInternal && !it.submitable && <button className="RwRead" onClick={e => _this.onClick(e, it)}>Read</button>}
+                        {it.isInternal && <span className="RwIntern">Internal</span>}
+                        <a target="_blank" className="RwSm" href={getEtherscanURL() + "address/" + it.location}>Smart Contract</a>
+                        {it.proposalAddress && parseInt(it.proposalAddress.toLowerCase().split("x").join("")) >0 &&  <a target="_blank" className="RwPa" href={getEtherscanURL() + "address/" + it.proposalAddress}>History</a>}
                     </li> )}
                     </ul>
             </div> 
