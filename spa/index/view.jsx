44b3974092456f254b1233bef886195f38c42dc9
alet89 <alet@risepic.com>
2019-11-02 13:26:33 +0100

    0.61

--- spa/index/view.jsx
+++ spa/index/view.jsx
@@ -25,14 +25,15 @@ var Index = React.createClass({
                     <figure>
                         <div className="GIFY"><iframe src="https://giphy.com/embed/cPHYguVPkcXBIM5tpX" width="100%" height="100%" style={{"position":"absolute"}} frameBorder="0" className="giphy-embed" allowFullScreen></iframe></div>
                     </figure>
-                    <h4>Deploy and Manage a Flexible Organization, without points of failure</h4>
+                    <h4>Deploy and Manage a DFO</h4>
+                    <p>The Decentralized Flexible Organization is a new concept, that reshapes how we’re building Decentralized Applications, solving some critical points of failure in the today’s Dapp ecosystem like the needs of a Legal Known Entity to Trust, the slowly Smart Contract Developing and the Centralized or Distributed Servers Needs.</p>
                     <div className="MainBtn">
                         <a target="_blank" href="https://medium.com/risepic/introducing-the-decentralized-flexible-organization-8c9e6fbab6d4">Manifesto</a>
-                        <a target="_blank" href="https://github.com/b-u-i-d-l">GitHub</a>
-                        <a target="_blank" href="https://gitcoin.co/grants/154/decentralized-flexible-organization">GitCoin</a>
+                        <a target="_blank" href="https://github.com/b-u-i-d-l">Code</a>
+                        <a target="_blank" href="https://gitcoin.co/grants/154/decentralized-flexible-organization">Support</a>
                     </div>
                     <div className="MainLoad">
-                       <input ref={ref => this.address = ref } placeholder="Enter your ethereum address here..." /> 
+                       <input ref={ref => this.address = ref } placeholder="DFO ETH Address" /> 
                        <button type="button" onClick={this.load}>Load</button>
                        <a className="MainDeploy" href="javascript:;" onClick={this.deploy}>or deploy a new <span className="BOLD">Decentralized Flexible Organization</span></a>
                     </div>
