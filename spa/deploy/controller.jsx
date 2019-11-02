c4af153c9b0d24430e5fa7ea540793e13bafbce5
alet89 <alet@risepic.com>
2019-11-02 18:30:07 +0100

    Update controller.jsx

--- spa/deploy/controller.jsx
+++ spa/deploy/controller.jsx
@@ -5,16 +5,17 @@ var DeployController = function (view) {
     context.deploy = async function deploy(data) {
         context.view.emit('message');
         var errors = [];
-        !data.dfoName && errors.push('DFO Name is mandatory');
-        !data.tokenSymbol && errors.push('Token Symbol is mandatory');
-        data.tokenDecimals < 1 && errors.push('Token decimals must be a number greater than 1');
-        data.tokenTotalSupply < 1 && errors.push('Token total supply must be a number greater than 1');
-        data.surveyLength < 1 && errors.push('Survey Length must be a number greater than 1');
-        !data.surveyValidationRulesAddress && errors.push('Survey Validation Rules Address is mandatory and must be a valid ethereum address');
+        !data.dfoName && errors.push('Insert a valid DFO Name');
+        !data.tokenSymbol && errors.push('Insert a valid Token Symbol');
+        data.tokenDecimals < 1 && errors.push('Token Decimals must be greater than 1');
+        data.tokenTotalSupply < 1 && errors.push('Token Total Supply must be greater than 1');
+        data.surveyLength < 1 && errors.push('Survey Length must be greater than 1');
+        !data.surveyValidationRulesAddress && errors.push('Survey Validation Rules Address must be a valid Ethereum Address');
         if (errors.length > 0) {
             throw errors;
         }
         context.view.emit('message', 'Transaction 1 of 3 - Creating MVD Functionality Proposal Factory...', 'info');
+        context.view.emit("loader/toggle");
         var mvdFunctionalityProposalFactory = await createContract(window.context.mvdFunctionalityProposalFactoryAbi, window.context.mvdFunctionalityProposalFactoryBin);
         context.view.emit('message', 'Transaction 2 of 3 - Creating Survey Block Length...', 'info');
         var mvdBlockLengthProvider = await createContract(window.context.mvdBlockLengthProviderAbi, window.context.mvdBlockLengthProviderBin, data.surveyLength);
